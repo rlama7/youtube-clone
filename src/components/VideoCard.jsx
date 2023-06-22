0549ce8083e48099b2d506f855e0e5219d81f618
Ratna Lama <royalray7@gmail.com>
2023-06-22 15:27:13 -0700

    add typography, sidebar, feed, api & card

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import {
+  demoThumbnailUrl,
+  demoVideoTitle,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoChannelUrl,
+} from '../utils/constants';
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  console.log(videoId, snippet);
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
